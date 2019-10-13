3d1f67475062d5b2687dfbabf21e134b27538ee8
Andrei <rysiavetsandrei@gmail.com>
2019-10-13 12:34:36 +0200

    completed task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,33 @@
 class GuessingGame {
-    constructor() {}
+
+    constructor() {
+
+        this.start = 0;
+        this.end = 0;
+        this.pointer = null;
+    }
 
     setRange(min, max) {
 
+        this.start = min;
+        this.end = max;
     }
 
     guess() {
 
+        return this.pointer = Math.round((this.start + this.end) / 2);
     }
 
     lower() {
 
+        this.end = this.pointer;
     }
 
     greater() {
 
+        this.start = this.pointer;
     }
+
 }
 
 module.exports = GuessingGame;
